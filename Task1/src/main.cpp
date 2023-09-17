56fae656c358bef2813b8443ec8206551af0b625
y.vegner <y.vegner@g.nsu.ru>
2023-09-17 22:54:19 +0700

    nW

--- Task1/src/main.cpp
+++ Task1/src/main.cpp
@@ -1,5 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double factorial(int n) {
+    double result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+double power(double x, int n) {
+    double result = 1;
+    for (int i = 0; i < n; ++i) {
+        result *= x;
+    }
+    return result;
+}
+
+double sinTaylorSeries(double x, int terms) {
+    double result = 0;
+    for (int n = 0; n < terms; ++n) {
+        double term = power(-1, n) * power(x, 2 * n + 1) / factorial(2 * n + 1);
+        result += term;
+    }
+    return result;
+}
 
 int main() {
-	std::cout << "Hello";
+    double x = 5;
+    int N = 50000;
+
+    double sinX = sinTaylorSeries(x, N);
+
+    return 0;
 }
